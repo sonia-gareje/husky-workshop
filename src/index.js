430b30ee22de4c64650bc45e20411e9c4b538231
sonia-gareje <sonia.escapa@garajedeideas.com>
2025-11-27 14:52:32 +0100

    test

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@
 // STAGE 5: "Secret" committed to repo
 // ======================================================
 
-const SECRET_KEY = "12312asdasd3";
+const SECRET_KEY = "1231h451i62";
 const AWS_TOKEN = process.env.AWS_TOKEN;
 // eslint-disable-next-line no-console
 console.log(SECRET_KEY, AWS_TOKEN)
